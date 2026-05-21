b38c5107eef61a6cc47448c8d29b9ba82e10892e
JOSEV <jose.vargas@unillanos.edu.co>
2026-05-20 23:40:27 -0500

    que coja el icono de produccion en las plantillas de email

--- src/resources/views/emails/partials/logo.blade.php
+++ src/resources/views/emails/partials/logo.blade.php
@@ -1,15 +1,11 @@
-@php
-    $logoUrl = config('app.url') . '/img/logo-login.jpeg';
-@endphp
-
 <table width="100%" cellpadding="0" cellspacing="0" role="presentation">
     <tr>
         <td align="center" style="padding: 24px 0;">
             <img
-                src="{{ $logoUrl }}"
+                src="https://ecodata.metropolis.icu/img/logo-login.jpeg"
                 alt="EcoData"
                 width="160"
-                style="display: block; max-width: 160px; height: auto; border: 0;"
+                style="display:block;max-width:160px;height:auto;border:0;margin:0 auto;"
             >
         </td>
     </tr>
